c4ceac8e868981ad47283522a4df6cc59104f036
Oleksandr Gneushev <galexing@gmail.com>
2022-09-15 07:18:43 +0200

    first commit - add all functionality

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x9B2D39cBa1065fABF9cc20BDEb4B84fc8c7b33E3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
